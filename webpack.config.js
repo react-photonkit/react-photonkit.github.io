d16ab17ad5f59c6a62134c075f991c62fef669c2
ragingwind <ragingwind@gmail.com>
2016-08-16 11:37:59 +0900

    Update indent

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@
 const path = require('path')
 
 module.exports = {
-  module: {
+	module: {
 		preLoaders: [{
-      test: /\.js?/,
-      exclude: /node_modules/,
-      loader: 'xo-loader'
-    }],
+			test: /\.js?/,
+			exclude: /node_modules/,
+			loader: 'xo-loader'
+		}],
 		loaders: [{
 			test: /\.jsx?$/,
 			exclude: /node_modules/,
@@ -23,33 +23,33 @@ module.exports = {
 			test: /bootstrap\/js\//,
 			loader: 'imports?jQuery=jquery'
 		}, {
-      test: /\.json$/,
-      loader: 'json-loader'
-    }, {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?limit=1000000&mimetype=application/font-woff"
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "file-loader"
-    }]
-  },
-  entry: path.resolve(__dirname, 'app/app.jsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/',
-    filename: '[name].js'
-  },
+			test: /\.json$/,
+			loader: 'json-loader'
+		}, {
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url-loader?limit=1000000&mimetype=application/font-woff"
+		}, {
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file-loader"
+		}]
+	},
+	entry: path.resolve(__dirname, 'app/app.jsx'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: 'dist/',
+		filename: '[name].js'
+	},
 	xo: {
-    extends: ["xo", "xo-react"],
+		extends: ["xo", "xo-react"],
 		env: ['browser'],
-    rules: {
+		rules: {
 			'no-alert': 0,
 			'react/jsx-no-bind': [2, {
 				'ignoreRefs': true,
 				'allowArrowFunctions': true
 			}],
 			'react/require-optimization': 0,
-      "quote-props": ["error", "as-needed"]
-    }
-  }
+			"quote-props": ["error", "as-needed"]
+		}
+	}
 };
